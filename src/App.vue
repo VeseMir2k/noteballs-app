fca666fb3a05e1d41097d5c7c85e9a93e6d1b756
Adam Stępień <vesemir2k@gmail.com>
2024-09-03 00:53:44 +0200

    📝 (index.html): add data-theme attribute to set the theme to light for better user experience 🔧 (package.json): add bulma dependency to version 1.0.2 for styling components ✨ (App.vue): add Navbar component to layout the navigation bar and import Bulma CSS for styling ✨ (Navbar.vue): create a new Navbar component with responsive design for navigation 🔧 (main.js): remove import statement for main.css as it is not used ✨ (ViewNotes.vue): update the layout of ViewNotes component to include a form for adding new notes and display existing notes with edit and delete options

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <template>
-  <RouterLink to="/">Notes</RouterLink>
-  <RouterLink to="/stats">Stats</RouterLink>
-  <RouterView />
+  <Navbar />
+
+  <div class="container is-max-desktop px-2 py-4">
+    <RouterView />
+  </div>
 </template>
+
+<script setup>
+  // ~ imports
+  import Navbar from '@/components/Layout/Navbar.vue';
+</script>
+
+<style>
+  @import 'bulma/css/bulma.min.css';
+</style>
